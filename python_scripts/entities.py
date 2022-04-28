d2c3d6186e6c028a2ccd58a4df177368f05e7d87
Yaroslav <m2003578@edu.misis.ru>
2022-04-29 02:47:14 +0300

    fix merge conflict

--- python_scripts/entities.py
+++ python_scripts/entities.py
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 import cv2 as cv
 import numpy as np
 
 class Marker:
-=======
-import cv2
-import numpy as np
-
-class marker:
->>>>>>> 1d80a791a3865431e88396f9ca970d23401cda74
     def __init__(self, id, corners, name = ""):
         self.id = id
         self.name = name
         self.corners = corners.copy()
-<<<<<<< HEAD
         M = cv.moments(corners)
-=======
-        M = cv2.moments(corners)
->>>>>>> 1d80a791a3865431e88396f9ca970d23401cda74
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
         self.center = (cX, cY)
@@ -25,11 +14,7 @@ class marker:
     def __str__(self):
         return "ID: %x" % (self.id)
 
-<<<<<<< HEAD
 class DefinedMarker(Marker):
-=======
-class definedMarker(marker):
->>>>>>> 1d80a791a3865431e88396f9ca970d23401cda74
     def __init__(self, marker, rvec, tvec):
         self.__dict__ = marker.__dict__.copy()
         self.rvec = rvec[0]
@@ -38,10 +23,6 @@ class definedMarker(marker):
     def __str__(self):
         return "ID: %x Pose: %s  Ang: %s(deg)" % (self.id, str(self.tvec[0]), str(np.rad2deg(self.rvec[0])))
 
-<<<<<<< HEAD
 class Robot:
-=======
-class robot:
->>>>>>> 1d80a791a3865431e88396f9ca970d23401cda74
     def __init__(self):
         pass
